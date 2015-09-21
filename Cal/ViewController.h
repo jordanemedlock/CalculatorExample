1d426bc9ba73ecb22b960cac0fc44ad8a72deb9c
Jordan Medlock <jordanemedlock@gmail.com>
2015-09-21 10:10:51 -0600

    Added most functionality

--- Cal/ViewController.h
+++ Cal/ViewController.h
@@ -8,7 +8,18 @@
 
 #import <UIKit/UIKit.h>
 
+enum CardinalDirection {
+    HEAD, TAIL
+};
+
+
 @interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *displayedNumber;
+@property (nonatomic) long long currentNum;
+@property (nonatomic) long long newNumber;
+@property (strong, nonatomic) NSString * currentOp;
+@property (strong, nonatomic) NSDictionary * operations;
 
 
 @end
--- Cal/ViewController.m
+++ Cal/ViewController.m
@@ -8,6 +8,12 @@
 
 #import "ViewController.h"
 
+typedef long long (^oper)(long long, long long);
+
+#define BIOPP(OPP) ^(long long lhs, long long rhs) { \
+    return lhs OPP rhs; \
+} \
+
 @interface ViewController ()
 
 @end
@@ -16,12 +22,45 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.currentNum = 0;
+    
+    self.operations = @{
+                          @"+": BIOPP(+),
+                          @"-": BIOPP(-)
+                        };
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)operationPressed:(UIButton *)sender {
+    
+    NSString * opp = sender.titleLabel.text;
+    self.currentOp = opp;
+    self.newNumber = self.currentNum;
+    self.currentNum = 0;
+}
 
+- (IBAction)equalPressed:(UIButton *)sender {
+    oper opp = self.operations[self.currentOp];
+    if (opp) {
+        self.currentNum = opp(self.currentNum, self.newNumber);
+        self.displayedNumber.text = [NSString stringWithFormat:@"%lld",self.currentNum];
+    }
+}
+
+- (IBAction)numberButtonTouched:(UIButton *)sender {
+    
+    NSLog(@"%@", sender.titleLabel.text);
+    
+    int buttonNumber = [[sender.titleLabel.text substringToIndex:1] intValue];
+    
+    if (self.currentNum < (LLONG_MAX >> 4)) {
+        self.currentNum = buttonNumber + self.currentNum*10;
+    }
+    
+    
+    self.displayedNumber.text = [NSString stringWithFormat:@"%lld",self.currentNum];
+}
 @end
